8eda4f03f7567d1e60da4205815117cae444c0db
elshadayz21 <83093239+elshadayz21@users.noreply.github.com>
2022-03-10 20:52:31 +0300

    contents updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Prints written quote
+ *
+ * Return: zero on success
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ *main -  print quote using printf
+ *Description: very simple snippet
+ *Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);

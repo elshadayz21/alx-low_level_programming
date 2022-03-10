389f7842bb6418eacd972b0465bfb2bd007a1280
elshadayz21 <83093239+elshadayz21@users.noreply.github.com>
2022-03-10 20:57:24 +0300

    commented

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Prints written quote
+ *
+ * Return: zero on success
+ *
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
